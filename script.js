98e6c2b8cc854593b9143f7e86d4e73dd66aa4b6
Aditya Pandey <100739638+Aditya2006p@users.noreply.github.com>
2024-11-18 10:11:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,19 +37,19 @@ async function getWeather(city = null, lat = null, lon = null) {
 
             switch(data.weather[0].main){
                 case 'Clouds':
-                    weather_img.src = "Weather-forecasting-project/cloud.png";
+                    weather_img.src = "./cloud.png";
                     break;
                 case 'Clear':
-                    weather_img.src = "Weather-forecasting-project/clear.png";
+                    weather_img.src = "./clear.png";
                     break;
                 case 'Rain':
-                    weather_img.src = "Weather-forecasting-project/rain.png";
+                    weather_img.src = "./rain.png";
                     break;
                 case 'Mist':
-                    weather_img.src = "Weather-forecasting-project/mist.png";
+                    weather_img.src = "./mist.png";
                     break;
                 case 'Snow':
-                    weather_img.src = "Weather-forecasting-project/snow.png";
+                    weather_img.src = "./snow.png";
                     break;
             }
 
